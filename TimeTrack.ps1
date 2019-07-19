a6b80185f5e7e41bf477134ea3a5f251ce128afd
Chuck Mindrup <chuck.mindrup@realpage.com>
2019-07-19 16:51:37 -0400

    fixed summary calculation issues

--- TimeTrack.ps1
+++ TimeTrack.ps1
@@ -56,7 +56,7 @@ function ManageLogFile ($FileName, $Activity, $TaskName, $VerbosePreference = "S
 }
 
 # ************* Script Body ********************
-$VerbosePreference = "Continue"
+$VerbosePreference = "SilentlyContinue"
 $TaskListFile = "$([Environment]::GetFolderPath('MyDocuments'))\TaskList-$((Get-Date).ToString("MMddyyyy")).txt"
 $TaskLogFile = "$([Environment]::GetFolderPath('MyDocuments'))\TaskLog-$((Get-Date).ToString("MMddyyyy")).txt"
 $TaskList = ManageListFile -FileName $TaskListFile
@@ -98,26 +98,28 @@ do {
 # Present a summary
 $Totals = @()
 $TaskLogItems = Import-Csv -Path $TaskLogFile
+$prevTimeStamp = $null
 foreach ($TaskLogItem in $TaskLogItems) {
+    Write-Verbose "$($TaskLogItem.TimeStamp) $($TaskLogItem.Activity) $($TaskLogItem.Taskname) ::: $($prevTimeStamp)"
     if ($prevTimeStamp) {
         $MatchFound = $false
         foreach ($total in $Totals) {
             if ($total.TaskName -eq $TaskLogItem.TaskName) {
                 $total.Minutes = $total.Minutes + (NEW-TIMESPAN -Start $prevTimeStamp -End $TaskLogItem.TimeStamp)
                 $MatchFound = $true
+                Write-Verbose "Added $((NEW-TIMESPAN -Start $prevTimeStamp -End $TaskLogItem.TimeStamp)) minutes to $($total.TaskName)"
                 break
             }
         }
         if (!($MatchFound)) {
             $TempObj = New-Object System.Object
             $TempObj | Add-Member -type NoteProperty -Name TaskName -Value $TaskLogItem.TaskName
-            $TempObj | Add-Member -type NoteProperty -Name Minutes -Value $timeSpan
+            $TempObj | Add-Member -type NoteProperty -Name Minutes -Value (NEW-TIMESPAN -Start $prevTimeStamp -End $TaskLogItem.TimeStamp)
             $Totals += $TempObj
             Remove-Variable TempObj
+            Write-Verbose "created $($total.TaskName) with $((NEW-TIMESPAN -Start $prevTimeStamp -End $TaskLogItem.TimeStamp)) minutes"
         }
     }
     $prevTimeStamp = $TaskLogItem.TimeStamp
-    #$currTask = $TaskLogItem.name
-
 }
 $Totals | Out-GridView
